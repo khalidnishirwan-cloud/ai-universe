759afeb249562707f1489e3f2e4d1400ccbb4b8d
khalidnishirwan-cloud <khalidnishirwan@gmail.com>
2026-03-20 00:34:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,25 @@ import sqlite3
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+from flask import Flask, render_template, request, jsonify, redirect
+import sqlite3
 
+app = Flask(__name__)
+
+def init_db():
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT,
+            password TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+init_db()
 @app.route('/')
 def home():
     return render_template('index.html')
